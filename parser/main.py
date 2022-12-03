e4eb7df0b49428df06cbb3771e0376aca5ace108
anton-0 <anton.delinac@gmail.com>
2022-12-03 22:53:59 +0100

    added parser for categories and updated product fields initialization

--- parser/main.py
+++ parser/main.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import random
 
 
 def get_element(p, keys: [str]):
@@ -20,6 +21,26 @@ def parse_element(el):
     return el
 
 
+def set_product_fields(csv_data, prod_data):
+    csv_data['Show price'] = 1
+    csv_data['Available for order'] = 1
+    csv_data['Quantity'] = random.randint(0, 10)
+    csv_data['Accessories'] = "accessories field test"
+    csv_data['Low stock level'] = 2
+    csv_data['Text when in stock'] = "Product available"
+    csv_data['Text when backorder allowed'] = "Product out of stock"
+    # available values: new, used, refurbished
+    csv_data['Condition'] = "new"
+    # available values: both, catalog, search, none
+    csv_data['Visibility'] = "both"
+    # 1: PL Standard Rate (23%)
+    csv_data['Tax rules ID'] = 1
+    # price tax excl. = price tax incl. / (1 + tax)
+    csv_data['Price tax excluded'] = round(float(csv_data['Price tax excluded'].replace(',', '.')) / 1.23, 2)
+    if csv_data['On sale']:
+        csv_data['Discount amount'] = round(float(prod_data['price'].replace(',', '.')) - float(prod_data['final_price'].replace(',', '.')), 2)
+
+
 def products_to_csv():
     with open('products_import.csv', 'w+', encoding='utf-8', newline='') as csv_file,\
             open('../scrapper/products.json', 'r', encoding='utf-8') as products_file,\
@@ -30,29 +51,29 @@ def products_to_csv():
         writer.writerow(list(data_labels.keys()))
 
         for p in products['data']:
-            row = []
-            for el_path in data_labels.values():
+            row = {}
+            for key, el_path in data_labels.items():
                 el = get_element(p, el_path)
-                el = parse_element(el)
-                row.append(el)
-            writer.writerow(row)
+                row[key] = parse_element(el)
+            set_product_fields(row, p)
+            writer.writerow(list(row.values()))
+
+
+def set_category_fields(csv_data, parent_category):
+    csv_data["Root category"] = 0
+    csv_data["Parent category"] = "Home" if parent_category is None else parent_category['name']
 
 
 def parse_child_categories(writer: csv.writer, data_labels: dict, parent: dict):
-    if 'children' not in parent:
+    if 'children' not in parent or parent['level'] == 2:
         return
     for c in parent['children']:
-        row = []
+        row = {}
         for key, el_path in data_labels.items():
-            if key == "Root category":
-                el = 0
-            elif key == "Parent category":
-                el = parent['name']
-            else:
-                el = get_element(c, el_path)
-                el = parse_element(el)
-            row.append(el)
-        writer.writerow(row)
+            el = get_element(c, el_path)
+            row[key] = parse_element(el)
+        set_category_fields(row, parent)
+        writer.writerow(list(row.values()))
         parse_child_categories(writer, data_labels, c)
 
 
@@ -61,20 +82,17 @@ def categories_to_csv():
             open('../scrapper/categories.json', 'r', encoding='utf-8') as cat_file, \
             open('categories_labels.json', 'r') as labels_file:
         data_labels = json.load(labels_file)
-        categories = json.load(cat_file)
+        categories = [json.load(cat_file)]
         writer = csv.writer(csv_file, delimiter=';')
         writer.writerow(list(data_labels.keys()))
 
         for c in categories:
-            row = []
+            row = {}
             for key, el_path in data_labels.items():
-                if key == "Root category":
-                    el = 1 if not c['level'] else 0
-                else:
-                    el = get_element(c, el_path)
-                    el = parse_element(el)
-                row.append(el)
-            writer.writerow(row)
+                el = get_element(c, el_path)
+                row[key] = parse_element(el)
+            set_category_fields(row, None)
+            writer.writerow(list(row.values()))
             parse_child_categories(writer, data_labels, c)
 
 
