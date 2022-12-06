79e3daf94a98ef9e2a48482c156cc4fc767e9149
Jakub Dajczak <jakub.dajczak@usable.pl>
2022-12-06 01:22:03 +0100

    tak

--- website-tests/main.py
+++ website-tests/main.py
@@ -6,11 +6,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-from random import choice, sample
-
-
-WEBSITE = "https://localhost:4433"
+from random import choice, randint
 
 
 class Website(object):
@@ -26,14 +25,27 @@ class Website(object):
 
 
 class Customer:
+    WEBSITE = "https://localhost:4433"
+
     def __init__(self):
         options = Options()
         options.add_argument('ignore-certificate-errors')
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        self.driver.get(WEBSITE)
+        self.__get_website()
+
+    def __del__(self):
+        input('Press enter to exit')
+        self.driver.quit()
+
+    def __get_website(self):
+        self.driver.get(self.WEBSITE)
+
 
     def find_category(self):
-        categories = WebDriverWait(self.driver, 10).until(lambda x: x.find_element(By.CLASS_NAME,'category'))
+        try:
+            WebDriverWait(self.driver, 1).until(lambda x: x.find_element(By.CLASS_NAME,'category'))
+        except TimeoutException:
+            self.__get_website()
 
         categories = self.driver.find_elements(By.CLASS_NAME,'category')
 
@@ -46,21 +58,27 @@ class Customer:
         return None
 
     def find_product(self):
-        return choice(self.driver.find_elements(By.CLASS_NAME,'product-miniature'))        
+        products = self.driver.find_elements(By.CLASS_NAME,'product-miniature')
+        if products:
+            return choice(products)
+        return None       
 
     def buy_product(self, quantity):
-        click_counter = 0
-        try:
-            size = Select(self.driver.find_element(By.CLASS_NAME,'form-control'))
+        urls = set()
+        urls.add(self.driver.current_url)
+
+        size_form = self.driver.find_element(By.CLASS_NAME,'form-control')
+
+        if size_form.tag_name == 'select':
+            size = Select(size_form)
             choice(size.options).click()
-            click_counter += 1
-        except:
-            pass
+            urls.add(self.driver.current_url)
 
         colors = self.driver.find_elements(By.CLASS_NAME,'input-color')
-        if len(colors) > 0:
+
+        if colors:
             choice(colors).click()
-            click_counter += 1
+            urls.add(self.driver.current_url)
 
         amount = self.driver.find_element(By.ID,'quantity_wanted')
 
@@ -71,28 +89,79 @@ class Customer:
 
         sleep(1)
         webdriver.ActionChains(self.driver).send_keys(Keys.ESCAPE).perform()
+        urls.add(self.driver.current_url)
 
-        for i in range(click_counter):
+        for _ in range(len(urls)-1):
             sleep(0.1)
             self.driver.back()
 
-    def buying_spree(self, quantity):
+    def buying_spree(self, quantity, products_range=(1,1)):
         for i in range(quantity):
             with Website(self.driver, self.find_category()):
                 subcategory = self.find_subcategory()
                 if subcategory:
                     with Website(self.driver, subcategory):
                         with Website(self.driver, self.find_product()):
-                            self.buy_product(1)
+                            self.buy_product(randint(*products_range))
                 else:
                     with Website(self.driver, self.find_product()):
-                        self.buy_product(1)
+                        self.buy_product(randint(*products_range))
 
-    def __del__(self):
-        input('Press enter to exit')
-        self.driver.quit()
+    def shopping_cart(self):
+        self.driver.find_element(By.CLASS_NAME,'cart-preview').click()
+        delete = self.driver.find_elements(By.CLASS_NAME,'remove-from-cart')
+        if delete:
+            choice(delete).click()
 
+    def checkout(self):
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME,'btn-primary')))
+        self.driver.find_element(By.CLASS_NAME,'btn-primary').click()
+
+        self.driver.find_element(By.ID,'field-address1').send_keys('ul. Testowa 4/420')
+        self.driver.find_element(By.ID,'field-postcode').send_keys('80-420')
+        self.driver.find_element(By.ID,'field-city').send_keys('Testowo')
+
+        self.driver.find_element(By.NAME,'confirm-addresses').click()
+
+        self.driver.find_element(By.CSS_SELECTOR, "label[for='delivery_option_2']").click()
+
+        self.driver.find_element(By.NAME,'confirmDeliveryOption').click()
+
+        self.driver.find_element(By.ID,'payment-option-1').click()
+        self.driver.find_element(By.ID,'conditions_to_approve[terms-and-conditions]').click()
+
+        self.driver.find_element(By.ID,'payment-confirmation').find_element(By.TAG_NAME,'button').click()
+
+    def __fill_account_form(self):
+        self.driver.find_element(By.CLASS_NAME,'radio-inline').click()
+        self.driver.find_element(By.ID,'field-firstname').send_keys('Jan')
+        self.driver.find_element(By.ID,'field-lastname').send_keys('Kowalski')
+        self.driver.find_element(By.ID,'field-email').send_keys('jan.kowal@gmail.com')
+        self.driver.find_element(By.ID,'field-password').send_keys('2uPer4as1o')
+        self.driver.find_element(By.ID,'field-birthday').send_keys('2000-01-01')
+
+    def create_account(self):
+        self.driver.find_element(By.CLASS_NAME,'user-info').click()
+        self.driver.find_element(By.CLASS_NAME,'no-account').click()
+
+        self.__fill_account_form()
+
+        self.driver.find_element(By.NAME,'customer_privacy').click()
+
+        self.driver.find_element(By.CLASS_NAME,'btn-primary').click()
+
+    def check_order_status(self):
+        self.__get_website()
+        self.driver.find_element(By.CLASS_NAME,'user-info').find_element(By.CLASS_NAME,'account').click()
+        self.driver.find_element(By.ID,'history-link').click()
+
+        self.driver.find_element(By.CLASS_NAME,'view-order-details-link').click()
 
 if __name__ == "__main__":
-    Customer().buying_spree(10)
+    customer = Customer()
+    customer.create_account()
+    customer.buying_spree(5,(1,4))
+    customer.shopping_cart()
+    customer.checkout()
+    customer.check_order_status()
     
